533b3770c486189cc93f4d4e72ff7b651f5ef39b
anjmao <andzej.mac@gmail.com>
2017-07-13 09:23:08 +0300

    Remove problematic stack-trace for travis

--- karma-shim.js
+++ karma-shim.js
@@ -6,7 +6,6 @@ require('reflect-metadata');
 require('ts-helpers');
 
 require('zone.js/dist/zone');
-require('zone.js/dist/long-stack-trace-zone');
 require('zone.js/dist/proxy');
 require('zone.js/dist/sync-test');
 require('zone.js/dist/jasmine-patch');
