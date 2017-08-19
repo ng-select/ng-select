a99ea93d0ec5f0e1aa32df04f21065a221538926
anjmao <andzej.mac@gmail.com>
2017-08-19 17:49:08 +0300

    Add travis integration tests step

--- src/lib/src/ng-select.module.ts
+++ src/lib/src/ng-select.module.ts
@@ -1,11 +1,11 @@
-import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {CommonModule} from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
-import {NgSelectComponent} from './ng-select.component';
-import {AngOptionDirective, AngDisplayDirective} from './ng-templates.directive';
-import {AngSearchFocusDirective} from './ng-search-focus.directive';
-import {VirtualScrollModule} from './virtual-scroll.component';
+import { NgSelectComponent } from './ng-select.component';
+import { AngOptionDirective, AngDisplayDirective } from './ng-templates.directive';
+import { AngSearchFocusDirective } from './ng-search-focus.directive';
+import { VirtualScrollModule } from './virtual-scroll.component';
 
 @NgModule({
     declarations: [
