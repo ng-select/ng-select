794822b191faf0bb8962215b3dcee358d9519dd2
anjmao <andzej.mac@gmail.com>
2017-09-04 20:26:35 +0300

    Revert: autocomplete refactor

--- src/demo/app/select-autocomplete.component.ts
+++ src/demo/app/select-autocomplete.component.ts
@@ -1,25 +1,25 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
     selector: 'select-autocomplete',
-    template: `        
+    template: `
         <label>Search with autocomplete in Github accounts</label>
-        <ng-select bindLabel="login"
-                   bindValue="this"
-                   [items]="users"
-                   (search)="loadGithubUsers($event)"
-                   [itemsFunc]="loadGithubUsers.bind(this)"
+        <ng-select [items]="items"
+                   labelKey="login"
+                   valueKey="this"
+                   [typeahead]="typeahead"
                    [(ngModel)]="githubAccount">
-            
+
             <ng-template ng-option-tmp let-item="item">
                 <img [src]="item.avatar_url" width="20px" height="20px"> {{item.login}}
             </ng-template>
-            
+
         </ng-select>
         <p>
-            Selected github account: 
+            Selected github account:
             <span *ngIf="githubAccount">
                 <img [src]="githubAccount.avatar_url" width="20px" height="20px"> {{githubAccount.login}}
             </span>
@@ -29,14 +29,24 @@ import { Observable } from 'rxjs/Observable';
 export class SelectAutocompleteComponent {
 
     githubAccount: any;
-    users: any[];
+    items = [];
+    typeahead = new Subject<string>();
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) {
+        this.typeahead
+            .distinctUntilChanged()
+            .debounceTime(200)
+            .switchMap(term => this.loadGithubUsers(term))
+            .subscribe(items => {
+                this.items = items;
+            }, (err) => {
+                console.log(err);
+                this.items = [];
+            });
+    }
 
-    loadGithubUsers(event: { term: string }) {
-        this.http.get<any>(`https://api.github.com/search/users?q=${event.term}`).subscribe(rsp => {
-            this.users = rsp.items;
-        });
+    loadGithubUsers(term: string): Observable<any[]> {
+        return this.http.get<any>(`https://api.github.com/search/users?q=${term}`).map(rsp => rsp.items);
     }
 }
 
