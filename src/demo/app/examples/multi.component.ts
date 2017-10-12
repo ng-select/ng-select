3aadebca839aa481ddb32c639ca3400f634d26bb
varnastadeus <varnastadeus@gmail.com>
2017-10-12 23:41:51 +0300

    chore: CR fixes

--- src/demo/app/examples/multi.component.ts
+++ src/demo/app/examples/multi.component.ts
@@ -36,9 +36,9 @@ import { Observable } from 'rxjs/Observable';
             [(ngModel)]="selectedUsers">
 
             <ng-template ng-label-tmp let-item="item" let-unselect="unselect">
-                <div class="ng-value-label default">
-                    <img [src]="item.avatar_url" width="20px" height="20px"> {{item.login}}
-                    <span class="ng-clear right" (click)="unselect(item)" aria-hidden="true">×</span>
+                <div class="ng-value-wrapper default">
+                    <span class="ng-value-label"><img [src]="item.avatar_url" width="20px" height="20px"> {{item.login}}</span>
+                    <span class="ng-value-icon right" (click)="unselect(item)" aria-hidden="true">×</span>
                 </div>
             </ng-template>
 
