57463a4ba469ef83a2b6ec5842eb8ba2e1fef9d1
anjmao <andzej.maciusovic@gmail.com>
2017-10-20 20:36:21 +0300

    add github starts button

--- src/demo/app/layout/header.component.ts
+++ src/demo/app/layout/header.component.ts
@@ -8,7 +8,7 @@ import { Component, EventEmitter } from '@angular/core';
             <img src="https://angular.io/assets/images/logos/angular/angular.svg" width="32px" height="32px" />
             @ng-select/ng-select
         </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse"  aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
 
@@ -24,7 +24,6 @@ import { Component, EventEmitter } from '@angular/core';
             <a class="nav-link disabled" href="#">Disabled</a>
             </li>
             <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
             <div class="dropdown-menu" aria-labelledby="dropdown01">
                 <a class="dropdown-item" href="#">Action</a>
                 <a class="dropdown-item" href="#">Another action</a>
@@ -33,7 +32,13 @@ import { Component, EventEmitter } from '@angular/core';
             </li>-->
         </ul>
         <form class="form-inline my-2 my-lg-0">
-            <a class="github-button" data-size="large" href="https://github.com/ng-select/ng-select" aria-label="Follow @ng-select on GitHub">Follow @ng-select</a>
+            <!-- Place this tag where you want the button to render. -->
+            <a class="github-button"
+               href="https://github.com/ng-select/ng-select"
+               data-icon="octicon-star"
+               data-size="large"
+               data-show-count="true"
+               aria-label="Star ng-select/ng-select on GitHub">Star</a>
         </form>
         </div>
     </nav>
