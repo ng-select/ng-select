710126abd8469cebc7b75a509dbbd3d14259e56d
Andzej Maciusovic <andzejmaciusovic@Andzejs-MacBook-Pro.local>
2017-10-20 09:37:14 +0300

    Add header in demo page

--- src/demo/app/app.module.ts
+++ src/demo/app/app.module.ts
@@ -16,6 +16,8 @@ import { SelectEventsComponent } from './examples/events.component';
 import { SelectMultiComponent } from './examples/multi.component';
 import { SelectAutocompleteComponent } from './examples/autocomplete.component';
 
+import { LayoutHeaderComponent } from './layout/header.component';
+
 const appRoutes: Routes = [
     {
         path: '',
@@ -55,7 +57,9 @@ const appRoutes: Routes = [
         ReactiveFormsComponent,
         SelectEventsComponent,
         SelectMultiComponent,
-        SelectAutocompleteComponent
+        SelectAutocompleteComponent,
+
+        LayoutHeaderComponent
     ],
     bootstrap: [AppComponent]
 })
