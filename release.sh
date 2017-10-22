bbf44f7726a5de5a75af397889fd10d4fac09a64
anjmao <andzej.maciusovic@gmail.com>
2017-10-22 16:58:50 +0300

    fix release script

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git pull
 cd ./src
-yarn run version
+node ../node_modules/standard-version/bin/cli.js --infile ../CHANGELOG.md
 cd ..
 yarn run build
 
@@ -9,5 +9,4 @@ read -p "Check changelog and press enter to push tags" tags
 git push --follow-tags origin master
 
 read -p "One more thing. Press enter to release to npm" npm
-cd ./dist
-npm publish --access=public
+npm publish dist --access=public
