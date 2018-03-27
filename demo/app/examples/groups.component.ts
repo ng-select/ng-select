0dd11c485583195e1a03fa56eb42d1bddbb570a3
Andžej Maciusovič <andzej.maciusovic@gmail.com>
2018-03-27 11:03:51 +0300

    feat: allow group by expression (#385)
    closes #375

--- demo/app/examples/groups.component.ts
+++ demo/app/examples/groups.component.ts
@@ -8,7 +8,7 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
             ng-select supports grouping flat array of objects by providing <b>groupBy</b> input
         </p>
 
-        <label>Default</label>
+        <label>Default group by key</label>
         ---html,true
         <ng-select [items]="accounts"
                 bindLabel="name"
@@ -22,38 +22,56 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
             <small>Selected: {{selectedAccount | json}}</small>
         </p>
 
-        <hr />
-        <label>With selectable groups</label>
+        <label>Group by function expression</label>
         ---html,true
         <ng-select [items]="accounts2"
                 bindLabel="name"
-                groupBy="country"
-                [selectableGroup]="true"
+                bindValue="name"
+                [groupBy]="groupByFn"
+                [multiple]="true"
                 [(ngModel)]="selectedAccount2">
         </ng-select>
         ---
         <p>
             <small>Selected: {{selectedAccount2 | json}}</small>
         </p>
+
+        <hr />
+        <label>With selectable groups</label>
+        ---html,true
+        <ng-select [items]="accounts3"
+                bindLabel="name"
+                groupBy="country"
+                [selectableGroup]="true"
+                [(ngModel)]="selectedAccount3">
+        </ng-select>
+        ---
+        <p>
+            <small>Selected: {{selectedAccount3 | json}}</small>
+        </p>
     `
 })
 export class SelectGroupsComponent {
     selectedAccount = ['Samantha'];
     accounts = [
-        { name: 'Adam', email: 'adam@email.com', age: 12, country: 'United States' },
-        { name: 'Samantha', email: 'samantha@email.com', age: 30, country: 'United States' },
-        { name: 'Amalie', email: 'amalie@email.com', age: 12, country: 'Argentina' },
-        { name: 'Estefanía', email: 'estefania@email.com', age: 21, country: 'Argentina' },
-        { name: 'Adrian', email: 'adrian@email.com', age: 21, country: 'Ecuador' },
-        { name: 'Wladimir', email: 'wladimir@email.com', age: 30, country: 'Ecuador' },
-        { name: 'Natasha', email: 'natasha@email.com', age: 54, country: 'Ecuador' },
-        { name: 'Nicole', email: 'nicole@email.com', age: 43, country: 'Colombia' },
-        { name: 'Michael', email: 'michael@email.com', age: 15, country: 'Colombia' },
-        { name: 'Nicolás', email: 'nicole@email.com', age: 43, country: 'Colombia' }
+        { name: 'Adam', email: 'adam@email.com', age: 12, country: 'United States', child: { state: 'Active' } },
+        { name: 'Samantha', email: 'samantha@email.com', age: 30, country: 'United States',  child: { state: 'Active' } },
+        { name: 'Amalie', email: 'amalie@email.com', age: 12, country: 'Argentina', child: { state: 'Active' } },
+        { name: 'Estefanía', email: 'estefania@email.com', age: 21, country: 'Argentina', child: { state: 'Active' } },
+        { name: 'Adrian', email: 'adrian@email.com', age: 21, country: 'Ecuador', child: { state: 'Active' } },
+        { name: 'Wladimir', email: 'wladimir@email.com', age: 30, country: 'Ecuador', child: { state: 'Inactive' } },
+        { name: 'Natasha', email: 'natasha@email.com', age: 54, country: 'Ecuador', child: { state: 'Inactive' } },
+        { name: 'Nicole', email: 'nicole@email.com', age: 43, country: 'Colombia', child: { state: 'Inactive' } },
+        { name: 'Michael', email: 'michael@email.com', age: 15, country: 'Colombia', child: { state: 'Inactive' } },
+        { name: 'Nicolás', email: 'nicole@email.com', age: 43, country: 'Colombia', child: { state: 'Inactive' } }
     ];
 
     accounts2 = this.accounts.slice();
-    selectedAccount2 = this.accounts2[1];
+    selectedAccount2 = ['Natasha'];
+    groupByFn = (item) => item.child.state;
+
+    accounts3 = this.accounts.slice();
+    selectedAccount3 = this.accounts3[1];
 
     ngOnInit() {
 
---
+++ integration_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+BASE_DIR=./integration/node_modules
+yarn clean
+yarn build
+mkdir -p $BASE_DIR
+mkdir -p ${BASE_DIR}/@ng-select
+mkdir -p ${BASE_DIR}/@ng-select/ng-select
+cp -R ./dist ${BASE_DIR}/@ng-select/ng-select
+cd ./integration
+yarn install
+yarn build
+yarn e2e
