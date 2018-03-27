0c3c4052034c373aafd696f801f0e7aa1aa19090
Andzej Maciusovic <andzej.maciusovic@gmail.com>
2018-03-27 08:58:15 +0300

    use relative paths

--- integration_test.sh
+++ integration_test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 yarn clean
 yarn build
-mkdir integration/node_modules/@ng-select
-mkdir integration/node_modules/@ng-select/ng-select
-cp -R dist/* integration/node_modules/@ng-select/ng-select
-cd integration
+mkdir ./integration/node_modules/@ng-select
+mkdir ./integration/node_modules/@ng-select/ng-select
+cp -R ./dist/* ./integration/node_modules/@ng-select/ng-select
+cd ./integration
 yarn build
 yarn e2e
