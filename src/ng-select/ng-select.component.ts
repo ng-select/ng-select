baa9f3047e1f8028831ee09b6031c1ed89dd6f06
Andzej Maciusovic <andzej.maciusovic@gmail.com>
2018-04-12 18:28:23 +0300

    rename typeahead to ng-select-typeahead

--- src/ng-select/ng-select.component.ts
+++ src/ng-select/ng-select.component.ts
@@ -66,7 +66,7 @@ export type CompareWithFn = (a: any, b: any) => boolean;
     host: {
         'role': 'listbox',
         'class': 'ng-select',
-        '[class.ng-single]': '!multiple',
+        '[class.ng-select-single]': '!multiple',
     }
 })
 export class NgSelectComponent implements OnDestroy, OnChanges, AfterViewInit, ControlValueAccessor {
@@ -95,10 +95,10 @@ export class NgSelectComponent implements OnDestroy, OnChanges, AfterViewInit, C
     @Input() virtualScroll = false;
     @Input() selectableGroup = false;
     @Input() searchFn = null;
-    @Input() @HostBinding('class.typeahead') typeahead: Subject<string>;
-    @Input() @HostBinding('class.ng-multiple') multiple = false;
-    @Input() @HostBinding('class.taggable') addTag: boolean | AddTagFn = false;
-    @Input() @HostBinding('class.searchable') searchable = true;
+    @Input() @HostBinding('class.ng-select-typeahead') typeahead: Subject<string>;
+    @Input() @HostBinding('class.ng-select-multiple') multiple = false;
+    @Input() @HostBinding('class.ng-select-taggable') addTag: boolean | AddTagFn = false;
+    @Input() @HostBinding('class.ng-select-searchable') searchable = true;
 
     @Input()
     get compareWith() { return this._compareWith; }
