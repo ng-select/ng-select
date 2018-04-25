47f2e597bcb182c94a6e42c05d2765ba31be45ff
Dustin Poole <xonuzofa@gmail.com>
2018-04-25 11:15:59 -0500

    fix: load items from ng-option async  (#489)
    fixes #479

--- src/ng-select/ng-select.component.ts
+++ src/ng-select/ng-select.component.ts
@@ -197,7 +197,7 @@ export class NgSelectComponent implements OnDestroy, OnChanges, AfterViewInit, C
     }
 
     ngAfterViewInit() {
-        if (this.ngOptions.length > 0 && this.items.length === 0) {
+        if (this.items && this.items.length === 0) {
             this._setItemsFromNgOptions();
         }
     }
