4f4d6f184a085d3974fde0da7fa9d1141747ef38
Javier <javico2609@gmail.com>
2018-11-07 13:06:55 -0300

    fix: handler mousedown for clear event when used animation library

--- src/ng-select/ng-select.component.ts
+++ src/ng-select/ng-select.component.ts
@@ -280,7 +280,7 @@ export class NgSelectComponent implements OnDestroy, OnChanges, AfterViewInit, C
         }
         $event.stopPropagation();
 
-        if (target.className === 'ng-clear-wrapper') {
+        if (target.className.indexOf('ng-clear-wrapper') !== -1) {
             this.handleClearClick();
             return;
         }
