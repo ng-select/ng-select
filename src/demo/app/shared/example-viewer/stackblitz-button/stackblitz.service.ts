0a0f3d2e270d610c492c8c154ffd53f161336fde
varnastadeus <varnastadeus@gmail.com>
2019-07-08 15:53:36 +0300

    chore: fix demo stackblitz examples path

--- src/demo/app/shared/example-viewer/stackblitz-button/stackblitz.service.ts
+++ src/demo/app/shared/example-viewer/stackblitz-button/stackblitz.service.ts
@@ -3,8 +3,8 @@ import { Injectable } from '@angular/core';
 import sdk from '@stackblitz/sdk';
 import { Project } from '@stackblitz/sdk/typings/interfaces';
 
-const EXAMPLE_PATH = '/examples/';
-const TEMPLATE_PATH = '/assets/stackblitz/';
+const EXAMPLE_PATH = '/ng-select/examples/';
+const TEMPLATE_PATH = '/ng-select/assets/stackblitz/';
 
 const TEMPLATE_FILES = [
     'index.html',
