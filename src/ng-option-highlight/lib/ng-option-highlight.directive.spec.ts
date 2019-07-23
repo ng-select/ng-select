8a0d880f94785848985b3eae50ed92f80dbc018f
Gus Ireland <gireland@onemedical.com>
2019-07-22 23:42:41 -0700

    fix: use regex with multiple return to highlight multiple search terms (#1275)
    closes #739

--- src/ng-option-highlight/lib/ng-option-highlight.directive.spec.ts
+++ src/ng-option-highlight/lib/ng-option-highlight.directive.spec.ts
@@ -7,7 +7,9 @@ import { By } from '@angular/platform-browser';
     template: `
         <span id="test1" [ngOptionHighlight]="term">My text is highlighted</span>
         <span id="test2" [ngOptionHighlight]="term">My text is not highlighted</span>
-        <span id="test3" *ngIf="showNew" [ngOptionHighlight]="term">New label</span>
+        <span id="test3" [ngOptionHighlight]="term">My text is highlighted</span>
+        <span id="test4" [ngOptionHighlight]="term">My ťëxť is highlighted text</span>
+        <span id="test5" *ngIf="showNew" [ngOptionHighlight]="term">New label</span>
     `
 })
 class TestComponent {
@@ -27,16 +29,17 @@ describe('NgOptionHighlightDirective', () => {
         fixture.detectChanges();
     });
 
-    it('should have two elements with highlight directive', () => {
+    it('should have five elements with highlight directive', () => {
         const highlightDirectives = fixture.debugElement.queryAll(By.directive(NgOptionHighlightDirective));
-        expect(highlightDirectives.length).toBe(2);
+        expect(highlightDirectives.length).toBe(5);
     });
 
     it('should have one element with highlighted text when term matching', () => {
         const span = fixture.debugElement.query(By.css('#test1'));
         fixture.componentInstance.term = 'is high';
         fixture.detectChanges();
-        expect(span.nativeElement.querySelector('.highlighted').innerHTML).toBe('is high');
+        expect(span.nativeElement.querySelectorAll('.highlighted')[0].innerHTML).toBe('is');
+        expect(span.nativeElement.querySelectorAll('.highlighted')[1].innerHTML).toBe('high');
         expect(span.nativeElement.textContent).toBe('My text is highlighted');
     });
 
@@ -48,12 +51,29 @@ describe('NgOptionHighlightDirective', () => {
         expect(span.nativeElement.innerHTML).toBe('My text is not highlighted');
     });
 
+    it('should have multiple elements with highlighted text when term matching', () => {
+        const span = fixture.debugElement.query(By.css('#test3'));
+        fixture.componentInstance.term = 'text highlighted';
+        fixture.detectChanges();
+        expect(span.nativeElement.querySelectorAll('.highlighted')[0].innerHTML).toBe('text');
+        expect(span.nativeElement.querySelectorAll('.highlighted')[1].innerHTML).toBe('highlighted');
+        expect(span.nativeElement.textContent).toBe('My text is highlighted');
+    });
+
+    it('Highlights special characters', () => {
+        const span = fixture.debugElement.query(By.css('#test4'));
+        fixture.componentInstance.term = 'ťëxť';
+        fixture.detectChanges();
+        expect(span.nativeElement.querySelector('.highlighted').innerHTML).toBe('ťëxť');
+        expect(span.nativeElement.textContent).toBe('My ťëxť is highlighted text');
+    });
+
     it('should highlight text when label changed',  () => {
         fixture.componentInstance.term = 'new';
         fixture.detectChanges();
         fixture.componentInstance.showNew = true;
         fixture.detectChanges();
-        const span = fixture.debugElement.query(By.css('#test3'));
+        const span = fixture.debugElement.query(By.css('#test5'));
         expect(span.nativeElement.querySelector('.highlighted').innerHTML).toBe('New');
         expect(span.nativeElement.textContent).toBe('New label');
     });
