8d42162e7c43604d3746cb688db8f54b9131b605
Greg Bruening <gregbruening@fastmail.com>
2020-03-26 15:20:35 -0400

    feat: remove autocomplete attribute

--- src/ng-select/lib/ng-select.component.ts
+++ src/ng-select/lib/ng-select.component.ts
@@ -820,7 +820,6 @@ export class NgSelectComponent implements OnDestroy, OnChanges, AfterViewInit, C
             type: 'text',
             autocorrect: 'off',
             autocapitalize: 'off',
-            autocomplete: this.labelForId ? 'off' : this.dropdownId,
             ...this.inputAttrs
         };
 
