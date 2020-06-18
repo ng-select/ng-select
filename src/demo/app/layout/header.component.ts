f4cfe44eb6135791b7dde72140bc84b2dd9b0280
Greg Bruening <gregbruening@fastmail.com>
2020-06-18 09:11:16 -0400

    feat(themes): :fire: remove theme stylesheets as they are unused

--- src/demo/app/layout/header.component.ts
+++ src/demo/app/layout/header.component.ts
@@ -1,10 +1,4 @@
 import { AfterViewInit, Component, EventEmitter, Input, Output } from '@angular/core';
-// @ts-ignore
-import antDesignTheme from '../../../ng-select/themes/ant.design.theme.scss';
-// @ts-ignore
-import defaultTheme from '../../../ng-select/themes/default.theme.scss';
-// @ts-ignore
-import materialTheme from '../../../ng-select/themes/material.theme.scss';
 
 type langDir = 'ltr' | 'rtl';
 
@@ -27,18 +21,6 @@ type langDir = 'ltr' | 'rtl';
             </button>
 
             <div class="collapse navbar-collapse">
-                <div ngbDropdown class="d-inline-block">
-                    <button class="btn btn-outline-secondary btn-sm" style="width: 150px;"
-                            ngbDropdownToggle>{{theme}}</button>
-                    <div ngbDropdownMenu>
-                        <button (click)="setTheme('Default theme')" class="dropdown-item btn-sm">Default theme</button>
-                        <button (click)="setTheme('Material theme')" class="dropdown-item btn-sm">Material theme
-                        </button>
-                        <button (click)="setTheme('Ant Design theme')" class="dropdown-item btn-sm">Ant Design theme
-                        </button>
-                    </div>
-                </div>
-
                 <div ngbDropdown class="d-inline-block ml-2">
                     <button class="btn btn-outline-secondary btn-sm text-uppercase" style="width: 60px;"
                             ngbDropdownToggle>{{dir}}</button>
@@ -63,34 +45,12 @@ type langDir = 'ltr' | 'rtl';
         </nav>
     `
 })
-export class LayoutHeaderComponent implements AfterViewInit {
-    theme = 'Default theme';
+export class LayoutHeaderComponent {
     @Input() version: string;
     @Input() dir: langDir;
     @Output() dirChange = new EventEmitter<langDir>();
 
     private style: HTMLStyleElement;
-
-    ngAfterViewInit() {
-        setTimeout(() => {
-            this.style = document.createElement('style');
-            this.style.type = 'text/css';
-            this.style.id = 'MyStyleTag'
-            this.style.innerHTML = defaultTheme;
-            document.getElementsByTagName('head')[0].appendChild(this.style);
-        }, 100);
-    }
-
-    setTheme(theme) {
-        this.theme = theme;
-        if (this.theme === 'Default theme') {
-            this.style.innerHTML = defaultTheme;
-        } else if (this.theme === 'Material theme') {
-            this.style.innerHTML = materialTheme;
-        } else {
-            this.style.innerHTML = antDesignTheme;
-        }
-    }
 
     changeDirTo(dir: langDir) {
         this.dir = dir;
