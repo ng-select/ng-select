ng-select/ng-select

commit d195ccce414985f3128ebf53c697548c3abde43a

feat: support compareWith (#398)
* feat: support compareWith

src/ng-select/items-list.ts
@@ -69,10 +69,12 @@ export class ItemsList {
 
     findItem(value: any): NgOption {
         if (this._ngSelect.bindValue) {
-            return this._items.find(item => !item.hasChildren && this.resolveNested(item.value, this._ngSelect.bindValue) === value);
+            return this._items.find(item => !item.hasChildren &&
+                this._ngSelect.compareWith(this.resolveNested(item.value, this._ngSelect.bindValue), value));
         }
-        const index = this._items.findIndex(x => x.value === value);
-        return index > -1 ? this._items[index] :
+        const index = this._items.findIndex(x => this._ngSelect.compareWith(x.value, value));
+        return index > -1 ?
+            this._items[index] :
             this._items.find(item => !item.hasChildren && item.label && item.label === this.resolveNested(value, this._ngSelect.bindLabel));
     }
 

src/ng-select/ng-select.component.spec.ts
@@ -629,6 +629,26 @@ describe('NgSelectComponent', function () {
                     expect(fixture.componentInstance.select.selectedItems).toEqual(result);
                 }));
 
+                it('should select by compareWith function', fakeAsync(() => {
+                    const fixture = createTestingModule(
+                        NgSelectTestCmp,
+                        `<ng-select [items]="cities"
+                            bindLabel="name"
+                            placeholder="select value"
+                            [compareWith]="compareWith"
+                            [(ngModel)]="selectedCity">
+                        </ng-select>`);
+
+
+                    const city = { name: 'Vilnius', id: 7, district: 'Ozo parkas' };
+                    fixture.componentInstance.cities.push(city);
+                    fixture.componentInstance.cities = [...fixture.componentInstance.cities];
+                    fixture.componentInstance.selectedCity = { name: 'Vilnius', district: 'Ozo parkas' } as any;
+
+                    tickAndDetectChanges(fixture);
+                    expect(fixture.componentInstance.select.selectedItems[0].value).toEqual(city);
+                }));
+
                 it('should select selected when there is no items', fakeAsync(() => {
                     const fixture = createTestingModule(
                         NgSelectTestCmp,
@@ -2317,6 +2337,7 @@ class NgSelectTestCmp {
     filter = new Subject<string>();
     selectOnTab = true;
 
+
     citiesLoading = false;
     selectedCityId: number;
     selectedCityIds: number[];
@@ -2345,6 +2366,8 @@ class NgSelectTestCmp {
             id: 5, name: term, valid: true
         });
     }
+
+    compareWith = (a, b) => a.name === b.name && a.district === b.district;
 
     toggleVisible() {
         this.visible = !this.visible;

src/ng-select/ng-select.component.ts
@@ -49,6 +49,7 @@ import { ConsoleService } from './console.service';
 export const NG_SELECT_DEFAULT_CONFIG = new InjectionToken<NgSelectConfig>('ng-select-default-options');
 export type DropdownPosition = 'bottom' | 'top' | 'auto';
 export type AddTagFn = ((term: string) => any | Promise<any>);
+export type CompareWithFn = (a: any, b: any) => boolean;
 
 @Component({
     selector: 'ng-select',
@@ -97,6 +98,15 @@ export class NgSelectComponent implements OnDestroy, OnChanges, AfterViewInit, C
     @Input() @HostBinding('class.taggable') addTag: boolean | AddTagFn = false;
     @Input() @HostBinding('class.searchable') searchable = true;
 
+    @Input()
+    get compareWith() { return this._compareWith; }
+    set compareWith(fn: CompareWithFn) {
+        if (!isFunction(fn)) {
+            throw Error('`compareWith` must be a function.');
+        }
+        this._compareWith = fn;
+    }
+
     // output events
     @Output('blur') blurEvent = new EventEmitter();
     @Output('focus') focusEvent = new EventEmitter();
@@ -139,6 +149,7 @@ export class NgSelectComponent implements OnDestroy, OnChanges, AfterViewInit, C
     private _primitive: boolean;
 
     private readonly _destroy$ = new Subject<void>();
+    private _compareWith = (a: any, b: any) => a === b;
     private _onChange = (_: NgOption) => { };
     private _onTouched = () => { };
 
@@ -533,7 +544,7 @@ export class NgSelectComponent implements OnDestroy, OnChanges, AfterViewInit, C
         }
     }
 
-    
+
 
     private _updateNgModel() {
         const model = [];

commit 8cd6b0397e4a18cb11589759e2943ef923c7841b

fix(compareWith): label is incorrectly used to determine selected item
This PR fixes a regression introduced in
d195ccce414985f3128ebf53c697548c3abde43a. The original code searched for
an ``NgOption`` first by identity of its value, but if it could not find
a match it then would search by label. The current code defaults to
using JavaScript's ``Array.find()`` method to look for a match by
identity *and* by label at the same time. Because ``Array.find()``
returns the first match this may yield the incorrect value.

In addition, this PR adds the attribute ``compareWithLabel`` to
``NgSelect`` so that a user can disable this surprising match by label
behavior if they wish.

src/ng-select/lib/items-list.ts
@@ -97,17 +97,22 @@ export class ItemsList {
         }
     }
 
-    findItem(value: any): NgOption {
-        let findBy: (item: NgOption) => boolean;
+    findItem(value: any): NgOption | undefined {
         if (this._ngSelect.compareWith) {
-            findBy = item => this._ngSelect.compareWith(item.value, value)
+            return this._items.find(item => this._ngSelect.compareWith(item.value, value));
         } else if (this._ngSelect.bindValue) {
-            findBy = item => !item.children && this.resolveNested(item.value, this._ngSelect.bindValue) === value
+            return this._items.find(
+                item => !item.children && this.resolveNested(item.value, this._ngSelect.bindValue) === value);
         } else {
-            findBy = item => item.value === value ||
-                !item.children && item.label && item.label === this.resolveNested(value, this._ngSelect.bindLabel)
+            const option = this._items.find(item => item.value === value);
+
+            if (!this._ngSelect.compareWithLabel) {
+                return option;
+            }
+
+            return option || this._items.find(
+                item => !item.children && item.label && item.label === this.resolveNested(value, this._ngSelect.bindLabel));
         }
-        return this._items.find(item => findBy(item));
     }
 
     addItem(item: any) {

src/ng-select/lib/ng-select.component.ts
@@ -78,6 +78,7 @@ export class NgSelectComponent implements OnDestroy, OnChanges, AfterViewInit, C
 
     @Input() bindLabel: string;
     @Input() bindValue: string;
+    @Input() compareWithLabel = true;
     @Input() markFirst = true;
     @Input() placeholder: string;
     @Input() notFoundText: string;

src/ng-select/lib/ng-select.component.spec.ts
@@ -1029,6 +1029,66 @@ describe('NgSelectComponent', () => {
                     expect(cmp.select.selectedItems[0].value).toEqual(cmp.cities[1]);
                 }));
 
+                it('should select by label when compareWithLabel is true/unset', fakeAsync(() => {
+                    const fixture = createTestingModule(
+                        NgSelectTestCmp,
+                        `<ng-select
+                            bindLabel="name"
+                            [items]="cities"
+                            [(ngModel)]="selectedCity"
+                            groupBy="state">
+                        </ng-select>`);
+
+                    const cities = [
+                        {id: 1, name: 'Glendale'},
+                        {id: 2, name: 'Pasadena'},
+                    ];
+                    fixture.componentInstance.cities = cities
+                    fixture.componentInstance.selectedCity = {id: -1, name: 'Pasadena'};
+                    tickAndDetectChanges(fixture);
+                    expect(fixture.componentInstance.select.selectedItems[0].value).toEqual(cities[1]);
+                }));
+
+                it('should select by value before label when compareWithLabel is true/unset', fakeAsync(() => {
+                    const fixture = createTestingModule(
+                        NgSelectTestCmp,
+                        `<ng-select
+                            bindLabel="name"
+                            [items]="cities"
+                            [(ngModel)]="selectedCity"
+                            groupBy="state">
+                        </ng-select>`);
+
+                    const glendaleAZ = {id: 1, name: 'Glendale', state: 'Arizona'};
+                    const glendaleCA = {id: 2, name: 'Glendale', state: 'California'};
+                    const cities = [glendaleAZ, glendaleCA];
+                    for (const order of [cities, cities.slice().reverse()]) {
+                        fixture.componentInstance.cities = order
+                        fixture.componentInstance.selectedCity = glendaleCA;
+                        tickAndDetectChanges(fixture);
+                        expect(fixture.componentInstance.select.selectedItems[0].value).toEqual(glendaleCA);
+                    }
+                }));
+
+                it('should select only by value if compareWithLabel is false', fakeAsync(() => {
+                    const fixture = createTestingModule(
+                        NgSelectTestCmp,
+                        `<ng-select
+                            bindLabel="name"
+                            [items]="cities"
+                            [(ngModel)]="selectedCity"
+                            [compareWithLabel]="false">
+                        </ng-select>`);
+
+                    const cities = [
+                        {id: 1, name: 'test'},
+                    ];
+                    fixture.componentInstance.cities = cities
+                    fixture.componentInstance.selectedCity = {id: -1, name: 'test'};
+                    tickAndDetectChanges(fixture);
+                    expect(fixture.componentInstance.select.selectedItems[0].value['id']).toEqual(-1);
+                }));
+
                 it('should select selected when there is no items', fakeAsync(() => {
                     const fixture = createTestingModule(
                         NgSelectTestCmp,