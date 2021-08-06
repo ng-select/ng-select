9696aa764d5ceb3cf446d87aabb49df429cdfb0a
Aly M <alydemah@gmail.com>
2021-08-06 15:43:13 +0200

    fix: search term doesn't return matching items
    Trim the search query input before searching in the items list.

--- src/ng-select/lib/ng-select.component.ts
+++ src/ng-select/lib/ng-select.component.ts
@@ -566,7 +566,7 @@ export class NgSelectComponent implements OnDestroy, OnChanges, AfterViewInit, C
             return;
         }
 
-        this.searchTerm = term;
+        this.searchTerm = term.trim();
         if (this._isTypeahead && (this._validTerm || this.minTermLength === 0)) {
             this.typeahead.next(term);
         }
