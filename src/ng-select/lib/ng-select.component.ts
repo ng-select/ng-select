0fe035fff9ffeb493fbf8b5ca510ddd92ea52082
varnastadues <varnastadeus@gmail.com>
2021-11-16 19:04:35 +0200

    chore: fix release build

--- src/ng-select/lib/ng-select.component.ts
+++ src/ng-select/lib/ng-select.component.ts
@@ -302,28 +302,28 @@ export class NgSelectComponent implements OnDestroy, OnChanges, OnInit, AfterVie
 
     handleKeyCode($event: KeyboardEvent) {
         switch ($event.which) {
-            case KeyCode.ArrowDown:
-                this._handleArrowDown($event);
-                break;
-            case KeyCode.ArrowUp:
-                this._handleArrowUp($event);
-                break;
-            case KeyCode.Space:
-                this._handleSpace($event);
-                break;
-            case KeyCode.Enter:
-                this._handleEnter($event);
-                break;
-            case KeyCode.Tab:
-                this._handleTab($event);
-                break;
-            case KeyCode.Esc:
-                this.close();
-                $event.preventDefault();
-                break;
-            case KeyCode.Backspace:
-                this._handleBackspace();
-                break
+        case KeyCode.ArrowDown:
+            this._handleArrowDown($event);
+            break;
+        case KeyCode.ArrowUp:
+            this._handleArrowUp($event);
+            break;
+        case KeyCode.Space:
+            this._handleSpace($event);
+            break;
+        case KeyCode.Enter:
+            this._handleEnter($event);
+            break;
+        case KeyCode.Tab:
+            this._handleTab($event);
+            break;
+        case KeyCode.Esc:
+            this.close();
+            $event.preventDefault();
+            break;
+        case KeyCode.Backspace:
+            this._handleBackspace();
+            break
         }
     }
 
--- src/ng-select/public-api.ts
+++ src/ng-select/public-api.ts
@@ -2,9 +2,9 @@
  * Public API Surface of ng-select
  */
 
-export { DropdownPosition, NgSelectComponent, SELECTION_MODEL_FACTORY } from './lib/ng-select.component';
+export { NgSelectComponent, SELECTION_MODEL_FACTORY } from './lib/ng-select.component';
 export { NgSelectModule } from './lib/ng-select.module';
-export { NgOption } from './lib/ng-select.types';
+export { NgOption, DropdownPosition } from './lib/ng-select.types';
 export { SelectionModel } from './lib/selection-model';
 export { NgSelectConfig } from './lib/config.service';
 export { NgOptionComponent } from './lib/ng-option.component';
