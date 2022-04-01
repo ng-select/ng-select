1e48fe33eb375bdf39f1d86f6afd8ada81e66f93
Tijmen van de Meent <tijmen.vdmeent@gmail.com>
2022-04-01 14:23:08 +0200

    feat: deselect option from dropdown in single select
    This feature enables deselecting an option from the
    dropdown when `multiple` is `false`. The user can
    click on a selected option, which will deselect that
    option instead of closing the dropdown.
    
    Fixes #2053

--- src/ng-select/lib/ng-select.component.ts
+++ src/ng-select/lib/ng-select.component.ts
@@ -150,6 +150,20 @@ export class NgSelectComponent implements OnDestroy, OnChanges, OnInit, AfterVie
         this._clearSearchOnAdd = value;
     };
 
+    @Input()
+    get deselectOnClick() {
+        if (isDefined(this._deselectOnClick)) {
+            return this._deselectOnClick;
+        } else if (isDefined(this.config.deselectOnClick)) {
+            return this.config.deselectOnClick;
+        }
+        return this.multiple;
+    };
+
+    set deselectOnClick(value) {
+        this._deselectOnClick = value;
+    };
+
     // output events
     @Output('blur') blurEvent = new EventEmitter();
     @Output('focus') focusEvent = new EventEmitter();
@@ -205,6 +219,7 @@ export class NgSelectComponent implements OnDestroy, OnChanges, OnInit, AfterVie
     private _pressedKeys: string[] = [];
     private _compareWith: CompareWithFn;
     private _clearSearchOnAdd: boolean;
+    private _deselectOnClick: boolean;
     private _isComposing = false;
 
     private get _editableSearchTerm(): boolean {
@@ -452,7 +467,7 @@ export class NgSelectComponent implements OnDestroy, OnChanges, OnInit, AfterVie
             return;
         }
 
-        if (this.multiple && item.selected) {
+        if (this.deselectOnClick && item.selected) {
             this.unselect(item);
         } else {
             this.select(item);
@@ -843,7 +858,7 @@ export class NgSelectComponent implements OnDestroy, OnChanges, OnInit, AfterVie
     }
 
     private _onSelectionChanged() {
-        if (this.isOpen && this.multiple && this.appendTo) {
+        if (this.isOpen && this.deselectOnClick && this.appendTo) {
             // Make sure items are rendered.
             this._cd.detectChanges();
             this.dropdownPanel.adjustPosition();
