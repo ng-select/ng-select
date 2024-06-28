550299e9b21aaba41aa7b5edd84fcacedc9671fb
pavankjadda <17564080+pavankjadda@users.noreply.github.com>
2024-06-28 21:22:47 +0000

    Deploying to gh-pages from @ ng-select/ng-select@e58d375a66fef6d8a92b6d845246de111d45d9a7 🚀

--- examples/bindings-default-example/bindings-default-example.component.ts
+++ examples/bindings-default-example/bindings-default-example.component.ts
@@ -1,21 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'bindings-default-example',
-    templateUrl: './bindings-default-example.component.html',
-    styleUrls: ['./bindings-default-example.component.scss']
+	selector: 'bindings-default-example',
+	templateUrl: './bindings-default-example.component.html',
+	styleUrls: ['./bindings-default-example.component.scss'],
 })
 export class BindingsDefaultExampleComponent implements OnInit {
+	defaultBindingsList = [
+		{ value: 1, label: 'Vilnius' },
+		{ value: 2, label: 'Kaunas' },
+		{ value: 3, label: 'Pavilnys', disabled: true },
+	];
 
-    defaultBindingsList = [
-        { value: 1, label: 'Vilnius' },
-        { value: 2, label: 'Kaunas' },
-        { value: 3, label: 'Pavilnys', disabled: true }
-    ];
+	selectedCity = null;
 
-    selectedCity = null;
-
-    ngOnInit() {
-        this.selectedCity = this.defaultBindingsList[0];
-    }
+	ngOnInit() {
+		this.selectedCity = this.defaultBindingsList[0];
+	}
 }
--- examples/template-display-example/template-display-example.component.ts
+++ examples/template-display-example/template-display-example.component.ts
@@ -3,34 +3,36 @@ import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 import { DataService } from '../data.service';
 
 @Component({
-    selector: 'template-display-example',
-    templateUrl: './template-display-example.component.html',
-    styleUrls: ['./template-display-example.component.scss']
+	selector: 'template-display-example',
+	templateUrl: './template-display-example.component.html',
+	styleUrls: ['./template-display-example.component.scss'],
 })
 export class TemplateDisplayExampleComponent implements OnInit {
+	peopleTypeahead = new EventEmitter<string>();
+	serverSideFilterItems = [];
+	selectedPeople;
 
-    peopleTypeahead = new EventEmitter<string>();
-    serverSideFilterItems = [];
-    selectedPeople;
+	constructor(private dataService: DataService) {}
 
-    constructor(private dataService: DataService) {
-    }
+	ngOnInit() {
+		this.serverSideSearch();
+	}
 
-    ngOnInit() {
-        this.serverSideSearch();
-    }
-
-    private serverSideSearch() {
-        this.peopleTypeahead.pipe(
-            distinctUntilChanged(),
-            debounceTime(300),
-            switchMap(term => this.dataService.getPeople(term))
-        ).subscribe(x => {
-            this.serverSideFilterItems = x;
-        }, (err) => {
-            console.log(err);
-            this.serverSideFilterItems = [];
-        });
-    }
-
+	private serverSideSearch() {
+		this.peopleTypeahead
+			.pipe(
+				distinctUntilChanged(),
+				debounceTime(300),
+				switchMap((term) => this.dataService.getPeople(term)),
+			)
+			.subscribe(
+				(x) => {
+					this.serverSideFilterItems = x;
+				},
+				(err) => {
+					console.log(err);
+					this.serverSideFilterItems = [];
+				},
+			);
+	}
 }
