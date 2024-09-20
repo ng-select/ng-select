620fe9f2338a70f19df9c2e8721d9bc6f7926bd5
pavankjadda <17564080+pavankjadda@users.noreply.github.com>
2024-09-20 16:46:06 +0000

    Deploying to gh-pages from @ ng-select/ng-select@06b33e35e62da9bad82c7c9aeee8a4a099268326 🚀

--- examples/fixed-placeholder-example/fixed-placeholder-example.component.ts
+++ examples/fixed-placeholder-example/fixed-placeholder-example.component.ts
@@ -3,10 +3,8 @@ import { Component } from '@angular/core';
 @Component({
 	selector: 'ng-fixed-placeholder-example',
 	templateUrl: './fixed-placeholder-example.component.html',
-	styleUrls: ['./fixed-placeholder-example.component.scss']
+	styleUrls: ['./fixed-placeholder-example.component.scss'],
 })
 export class FixedPlaceholderExampleComponent {
 	isPlaceholderFixed: boolean = true;
-
-
 }
