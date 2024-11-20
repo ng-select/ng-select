b8c6f874b6bbb014d158bff099a387cdb5a3cb5e
Maheswari Pureti <133873219+mahipureti@users.noreply.github.com>
2024-11-20 11:48:13 -0500

    fix(docs): fixes github docs issue (#2476)
    Co-authored-by: Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>

--- src/demo/app/app.module.ts
+++ src/demo/app/app.module.ts
@@ -1,9 +1,8 @@
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
 import { AppComponent } from './app.component';
 import { DataService } from './examples/data.service';
 import { ExamplesModule } from './examples/examples.module';
@@ -18,17 +17,13 @@ import { APP_BASE_HREF } from '@angular/common';
 @NgModule({
 	imports: [
 		BrowserModule,
-		HttpClientModule,
 		ExamplesModule,
 		NgbModule,
 		RouterModule.forRoot(appRoutes, {
 			useHash: true,
 		}),
 	],
-	providers: [
-		DataService,
-		// {provide: APP_BASE_HREF, useValue: '/ng-select/'}
-	],
+	providers: [provideHttpClient(), DataService, { provide: APP_BASE_HREF, useValue: '/ng-select/' }],
 	declarations: [
 		ExampleHostDirective,
 		AppComponent,
