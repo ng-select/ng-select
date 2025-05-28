baba92746813569dd0591d4488dab658daeac421
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2025-05-28 14:08:01 -0400

    fix(tests): reintroduce zone.js imports in test configuration

--- src/ng-select/test.ts
+++ src/ng-select/test.ts
@@ -1,7 +1,5 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-import 'zone.js';
-import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
 import { BrowserTestingModule, platformBrowserTesting } from '@angular/platform-browser/testing';
 
