c8200391f8e0f93efebfa2220f40fc5ee9cead43
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2025-05-29 09:58:03 -0400

    chore: update CI configuration and add Karma setup for ng-select and ng-option-highlight libraries

---
+++ src/ng-select/karma.conf.js
@@ -0,0 +1,35 @@
+// Karma configuration file for ng-select library
+module.exports = function (config) {
+	config.set({
+		basePath: '',
+		frameworks: ['jasmine', '@angular-devkit/build-angular'],
+		plugins: [
+			require('karma-jasmine'),
+			require('karma-chrome-launcher'),
+			require('karma-coverage'),
+			require('karma-jasmine-html-reporter'),
+			require('@angular/build'),
+		],
+		client: {
+			clearContext: false, // leave Jasmine Spec Runner output visible in browser
+		},
+		coverageReporter: {
+			dir: require('path').join(__dirname, '../../coverage/'),
+			subdir: 'ng-select',
+			reporters: [
+				{ type: 'html' },
+				{ type: 'lcovonly' },
+				{ type: 'text-summary' }
+			],
+			fixWebpackSourcePaths: true,
+		},
+		reporters: ['progress', 'kjhtml', 'coverage'],
+		port: 9876,
+		colors: true,
+		logLevel: config.LOG_INFO,
+		autoWatch: true,
+		browsers: ['Chrome'],
+		singleRun: false,
+		restartOnFileChange: true,
+	});
+};
