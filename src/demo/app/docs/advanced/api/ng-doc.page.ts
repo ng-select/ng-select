79bb4fbaf5f41397de241a402a8f210b9ff570e7
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2026-08-09 23:59:07 -0400

    refactor: remove ng-doc.api.ts and enhance documentation comments across components
    - Deleted the `ng-doc.api.ts` file to streamline the documentation structure.
    - Added detailed documentation comments to various components and services in the ng-select library, improving clarity and usability for developers.
    - Enhanced the `ng-option-highlight.directive.ts`, `config.service.ts`, `ng-option.component.ts`, and `ng-select.component.ts` with descriptive comments for better understanding of their functionalities.

---
+++ src/demo/app/docs/advanced/api/ng-doc.page.ts
@@ -0,0 +1,11 @@
+import { NgDocPage } from '@ng-doc/core';
+import AdvancedCategory from '../ng-doc.category';
+
+const ApiReferencePage: NgDocPage = {
+	title: 'API Reference',
+	mdFile: './index.md',
+	order: 2,
+	category: AdvancedCategory,
+};
+
+export default ApiReferencePage;
